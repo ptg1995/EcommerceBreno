1f116de62c17d1b6746bc61481e094304627e363
philip tavares <ptg1995@hotmail.com>
2022-05-21 19:03:24 -0300

    Construção do serviço das categorias dos produtos

---
+++ EcommerceBreno.ProductApi/Services/CategoryServices.cs
@@ -0,0 +1,62 @@
+﻿using EcommerceBreno.ProductApi.DTOs;
+using EcommerceBreno.ProductApi.Repositories;
+using AutoMapper;
+using EcommerceBreno.ProductApi.Models;
+
+namespace EcommerceBreno.ProductApi.Services
+{
+    public class CategoryServices : ICategoryService
+    {
+        private readonly ICategoryRepository? _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryServices(ICategoryRepository? categoryRepository, IMapper mapper)
+        {
+            this._categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task AddCategory(CategoryDTO categoryDTO)
+        {
+            var categoryEntity = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.Create(categoryEntity);
+        }
+
+        public async Task DeleteCategory(int id)
+        {
+            Category category = await _categoryRepository.GetById(id);
+            await _categoryRepository.Delete(category.CategoryId);
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetCategories()
+        {
+            var allCategories = await _categoryRepository.GetAll();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(allCategories);
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetCategoriesProducts()
+        {
+            var categoriesProducts = await _categoryRepository.GetCategoriesProducts();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesProducts);
+        }
+
+        public async Task<CategoryDTO> GetCategoryById(int id)
+        {
+            var categoryById = await _categoryRepository.GetById(id);
+            return _mapper.Map<CategoryDTO>(categoryById);
+        }
+
+        public async Task<CategoryDTO> GetCategoryByName(string name)
+        {
+            var categoryEntity = await _categoryRepository.GetAll();
+            var GetCategoryByName = categoryEntity.Where(c => c.Name == name) ;
+            return _mapper.Map<CategoryDTO>(GetCategoryByName);
+        }
+
+        public async Task UpdateCategory(CategoryDTO categoryDTO)
+        {
+            var categoryEntity = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.Update(categoryEntity);
+        }
+    }
+}
---
+++ EcommerceBreno.ProductApi/Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using EcommerceBreno.ProductApi.DTOs;
+namespace EcommerceBreno.ProductApi.Services;
+
+public interface ICategoryService
+{
+    Task<IEnumerable<CategoryDTO>> GetCategories();
+    Task<IEnumerable<CategoryDTO>> GetCategoriesProducts();
+    Task<CategoryDTO> GetCategoryById(int id);
+    Task<CategoryDTO> GetCategoryByName(string name);
+    Task AddCategory(CategoryDTO categoryDTO);
+    Task UpdateCategory(CategoryDTO categoryDTO);
+    Task DeleteCategory(int id);
+
+}
